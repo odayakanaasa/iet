18d5b2273fcb3cc4f56833127a70e4b0dcd0b18b
jessica <jessica.mcinchak@gmail.com>
2018-01-08 19:55:45 -0500

    render projects list as a table

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -2,26 +2,54 @@ import React from 'react'
 
 export default ({ data }) => {
   const projects = data.allAirtable.edges
-  const project = projects.map((p) => 
-    <li key={p.node.id}>{p.node.Name}</li>
-  )
 
   return (
     <div>
       <h1>Projects</h1>
-      <p>What we do:</p>
-      <ul>{project}</ul>
+      <p>What we work on:</p>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Description</th>
+            <th>Status</th>
+            <th>Link</th>
+          </tr>
+        </thead>
+        <tbody>
+          {projects.map(({ node }, i) =>
+            <tr key={i}>
+              <td>
+                {node.Name}
+              </td>
+              <td>
+                {node.Description}
+              </td>
+              <td>
+                {node.Status}
+              </td>
+              <td>
+                <a href={node.GitHub}>{node.GitHub}</a>
+              </td>
+            </tr>
+          )}
+        </tbody>
+      </table>
     </div>
   )
 }
 
 export const query = graphql`
 query ProjectsQuery {
-  allAirtable {
+  allAirtable(sort: { fields: [Status], order: ASC }) {
     edges {
       node {
         id
         Name
+        Description
+        Status
+        Type
+        GitHub
       }
     }
   }
