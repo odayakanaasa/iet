a3df63f9b230c4409084d93d0d32fb5afdb1c9d3
Jimmy McBroom <james.mcbroom@gmail.com>
2018-01-18 14:22:46 -0500

    light editing of copy

--- src/pages/about.js
+++ src/pages/about.js
@@ -35,11 +35,14 @@ const About = ({ data }) => {
     <div style={{ maxWidth: '800px' }}>
       <h1>About {data.site.siteMetadata.title}</h1>
       <p>The Office of Innovation & Emerging Technology (IET) is a team of programmers at the <a href="http://www.detroitmi.gov/">City of Detroit</a>.</p>
-      <p>We partner with City departments to build digital tools that solve civic problems and information challenges. Sometimes that looks like 
-        sunlighting a new type of City data and sometimes it means building web applications that help people communicate with the City in a new way. 
+      <p>We partner with City departments to build digital tools that solve civic problems and information challenges. Our projects can take a few forms:
+        <ul style={{margin: '1em'}}>
+          <li> sunlighting a new type of City data and making it accessible & understandable</li>
+          <li> building web applications that help people communicate with the City in a new way</li>
+        </ul>
         In any case, our projects aim to simplify internal work flows and make data more accessible to residents.</p>
-      <p>We value working in the open and in iterations. We approach projects by identifying a problem, developing prototypes, testing 
-        with users first-hand, and evaluating what makes a solution impactful (or not). We bring a fresh toolkit of open-source libraries and softwares 
+      <p>We value working iteratively and openly. We approach projects by identifying a problem, developing prototypes, testing 
+        with users first-hand, and evaluating the utility (or uselessness) of a solution. We bring a fresh toolkit of open-source libraries and softwares 
         to interact with the larger ecosystem of enterprise systems traditionally used throughout the City. All of our code is on <a href="https://github.com/cityofdetroit">Github</a>.</p>
       <p>Have an idea, project pitch, or just want to say hello?
         Email us at <a href="mailto:iet@detroitmi.gov">iet@detroitmi.gov</a>.</p>
@@ -51,15 +54,15 @@ const About = ({ data }) => {
       </div>
       <h3>History</h3>
       <p>The Office of Innovation & Emerging Technology (IET) was established in 2015 under the Department of Innovation & Technology's restructuring plan.
-        Near the same time, the City of Detroit signed into law it's first open data policy known as Government Open Data Access To 
-        All (GO DATA) or Executive Order 2015-2. This policy deemed all City information open by default, and that commitment to transparency shaped 
+        Around the same time, the mayor of the City of Detroit signed into law Detroit's first open data policy: <a href="https://data.detroitmi.gov/about"> Government Open Data Access To 
+        All (GO DATA) orExecutive Order 2015-2</a>. This policy deemed all City information open by default. That commitment to transparency shaped 
         how IET would prioritize working in the open and sharing our code, methods, and ideas along the way.</p>
       <p>Our current team came together in 2017. Detroit's Chief Innovation Officer (CIO) Beth Niblock acts as our 
         interim director. We appreciate these past team members:</p>
       <ul>
         <li>Garlin Gilchrist was the first Director of Innovation & Emerging Technology and helped us establish our footing and articulate a vision. 
           He left to run for local office in spring 2017</li>
-        <li>Joel Howrani-Heeres was Director of Open Data and activated our open data program. He became Detroit's Director of Sustainability in spring 2017</li> 
+        <li>Joel Howrani Heeres was Director of Open Data and activated our open data program. He became Detroit's Director of Sustainability in spring 2017</li> 
       </ul>
     </div>
   )
